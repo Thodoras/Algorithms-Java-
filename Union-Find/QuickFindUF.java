1098b462edd8e0b98e1f80f2fb26170457966856
Thodoras <theodore_topol@hotmail.com>
2015-09-06 17:29:08 +0300

    .

--- Union-Find/QuickFindUF.java
+++ Union-Find/QuickFindUF.java
@@ -2,29 +2,29 @@ public class QuickFindUF {
 	// A basic intuitive algorithm that connects nodes in O(N) time,
 	// and checks connectivity in O(1) time.
 
-	int[] roots;
+	int[] id;
 
 	public QuickFindUF(int num_nodes){
-		roots = new int[num_nodes];
+		id = new int[num_nodes];
 		for (int i = 0; i < num_nodes; i++)
-			roots[i] = i;
+			id[i] = i;
 	}
 
 	public boolean connetcted(int node1, int node2){
-		return roots[node1] == roots[node2];
+		return id[node1] == id[node2];
 	}
 
 	public void union(int node1, int node2){
-		int temp_root = roots[node1];
-		for (int i = 0; i < roots.length; i++)
-			if (roots[i] == temp_root)
-				roots[i] = roots[node2];
+		int temp_root = id[node1];
+		for (int i = 0; i < id.length; i++)
+			if (id[i] == temp_root)
+				id[i] = id[node2];
 	}
 
 	public int connected_comps(){
-		int[] temp_array = new int[roots.length];
+		int[] temp_array = new int[id.length];
 		for (int i = 0; i < temp_array.length; i++)
-			temp_array[roots[i]]++;
+			temp_array[id[i]]++;
 		int count = 0;
 		for (int i = 0; i < temp_array.length; i++)
 			if (temp_array[i] > 0)
---
+++ Union-Find/WeightedQuickUnionUF.java
@@ -0,0 +1,53 @@
+public class WeightedQuickUnionUF{
+	// Union - find algorith that uses extra space propotional to the graphs neumber
+	// of nodes but keeps union and find at O(logN).
+
+	int[] id;
+	int[] sz;
+
+	public WeightedQuickUnionUF(int num_nodes) {
+		id = new int[num_nodes];
+		sz = new int[num_nodes];
+		for (int i = 0; i < num_nodes; i++){
+			id[i] = i;
+			sz[i] = 1;
+		}
+	}
+
+	private int root(int node){
+		while (node != id[node]) {
+			//id[node] = id[id[node]];     //This way the node we are looking for will go up to the root flatening the tree.
+			node = id[node];
+		}
+		return node;
+	}
+
+	public boolean connected(int node1, int node2) {
+		return this.root(node1) == this.root(node2);
+	}
+
+	public void union(int node1, int node2) {
+		int root1 = this.root(node1);
+		int root2 = this.root(node2);
+		if (root1 == root2) return;
+		if (sz[root1] < sz[root2]){
+			id[root1] = id[root2];
+			sz[root2] += sz[root1]; 
+		}
+		else {
+			id[root2] = id[root1];
+			sz[root1] += sz[root2];
+		}
+	}
+
+	public int connected_comps(){
+		int[] temp_array = new int[id.length];
+		for (int i = 0; i < temp_array.length; i++)
+			temp_array[id[i]]++;
+		int count = 0;
+		for (int i = 0; i < temp_array.length; i++)
+			if (temp_array[i] > 0)
+				count++;
+		return count;
+	}
+}
