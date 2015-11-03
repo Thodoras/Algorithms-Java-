a9054886272b350ee3b136226a0828eaacabae01
Thodoras <theodoretopol@gmail.com>
2015-11-03 17:39:28 +0200

    .

---
+++ Graphs/GraphCC.java
@@ -0,0 +1,45 @@
+// A class that computes all the connected components of a given graph.
+
+public class GraphCC {
+
+	private boolean[] marked;
+	private int[] id;
+	private int count;
+
+	public GraphCC(Graph g) {
+		// Constructor.
+
+		marked = new boolean[g.V()];
+		id = new int[g.V()];
+		count = 0;
+		for (int i = 0; i < g.V(); i++) {
+			if (!marked[i]) {
+				dfs(g, i);
+				count++;
+			}
+		}
+	}
+
+	public int count() {
+		// return the number of components.
+
+		return count;
+	}
+
+	public int id(int vertex) {
+		// takes as input a vertex and returns the i-th cc component
+		// that belongs. 
+
+		return id[vertex];
+	}
+
+	private void dfs(Graph g, int v) {
+		marked[v] = true;
+		id[v] = count;
+		for (int vertex : g.adj(v)) {
+			if (!marked[vertex]) {
+				dfs(g,vertex);
+			}
+		}
+	}
+}
